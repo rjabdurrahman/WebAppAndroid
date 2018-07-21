c0e7f9fd4ca137bb7cb2cff2564ca01cfd4e9904
rjabdurrahman <md.rahman0296@gmail.com>
2018-07-21 16:22:04 +0600

    User Debit Credit Show by Calculating

--- pages/addtransaction/addtransaction.js
+++ pages/addtransaction/addtransaction.js
@@ -1,15 +1,32 @@
 app.controller('AddTransactionCntlr', function ($scope) {
-    $scope.getQueryVar = function (variable) {
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i = 0; i < vars.length; i++) {
-          var pair = vars[i].split("=");
-          if (pair[0] == variable) { return pair[1]; }
-        }
-        return (false);
-      };
-      var user = data.contacts.find((user) => {
-        return user.id == $scope.getQueryVar("uid");
-      });
-      $scope.user = user;
+  $scope.getQueryVar = function (variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+  };
+  var user = data.contacts.find((user) => {
+    return user.id == $scope.getQueryVar("uid");
+  });
+  $scope.user = user;
+  $scope.trans = function () {
+    return tranList.transactions.filter((tran) => {
+      return tran.userid == user.id;
+    });
+  }
+  $scope.debit = 0;
+  $scope.trans().filter((tran) => {
+    return tran.type == 1;
+  }).forEach(function (tran) {
+    $scope.debit += tran.amount;
+  });
+  $scope.credit = 0;
+  $scope.trans().filter((tran) => {
+    return tran.type == -1;
+  }).forEach(function (tran) {
+    $scope.credit += tran.amount;
+  });
 });
--- pages/userdetails/userdetails.js
+++ pages/userdetails/userdetails.js
@@ -18,4 +18,17 @@ app.controller('UserDetailsCntlr', function ($scope) {
       return tran.userid == user.id;
     });
   }
+  $scope.debit = 0;
+  $scope.trans().filter((tran) => {
+    return tran.type == 1;
+  }).forEach(function(tran) {
+    $scope.debit += tran.amount;
+  });
+  $scope.credit = 0;
+  $scope.trans().filter((tran) => {
+    return tran.type == -1;
+  }).forEach(function(tran) {
+    $scope.credit += tran.amount;
+  });
+  //End
 });
