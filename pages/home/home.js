cd6a20a4fdbba26cb1150fda4f75979f0a8e5856
rjabdurrahman <md.rahman0296@gmail.com>
2018-07-21 16:32:07 +0600

    Home Page Debit Credit Data Shown

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,9 +1,23 @@
 app.controller('HomeCntlr', function ($scope) {
   $scope.message = 'Hello from HomeController';
   $scope.trans = tranList.transactions;
-  $scope.user = function (id){
+  $scope.user = function (id) {
     return data.contacts.find((user) => {
       return user.id == id;
     }).name;
   }
+
+  $scope.debit = 0;
+  $scope.trans.filter((tran) => {
+    return tran.type == 1;
+  }).forEach((tran) => {
+    $scope.debit += tran.amount;
+  });
+  $scope.credit = 0;
+  $scope.trans.filter((tran) => {
+    return tran.type == -1;
+  }).forEach((tran) => {
+    $scope.credit += tran.amount;
+  });
+  $scope.net = $scope.debit -$scope.credit;
 });
