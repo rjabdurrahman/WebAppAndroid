fa99cccc19e43a202f5fca65e8e269a95c17b5bf
rjabdurrahman <md.rahman0296@gmail.com>
2018-08-02 12:22:17 +0600

    User Transaction list updated

--- pages/userdetails/userdetails.js
+++ pages/userdetails/userdetails.js
@@ -1,4 +1,16 @@
 app.controller('UserDetailsCntlr', function ($scope, $window) {
+  function $js(id) {
+    return document.getElementById(id);
+  }
+  function $lsGet(id) {
+    return localStorage.getItem(id);
+  }
+  function $lsSet(id, data) {
+    localStorage.setItem(id, data);
+  }
+  function $lsSetJ(id, data) {
+    localStorage.setItem(id, JSON.stringify(data));
+  }
   $scope.getQueryVar = function (variable) {
     var query = $window.location.search.substring(1);
     var vars = query.split("&");
@@ -9,7 +21,7 @@ app.controller('UserDetailsCntlr', function ($scope, $window) {
     return (false);
   };
   console.log($scope.getQueryVar("uid"));
-  function findContact(arr, data) {
+  function findArrEl(arr, data) {
     for (i = 0; i < arr.length; i++) {
       if (arr[i].id == data){
         return arr[i];
@@ -17,14 +29,26 @@ app.controller('UserDetailsCntlr', function ($scope, $window) {
       }
     }
   }
-  var allcontacts = JSON.parse(localStorage.getItem("contactList")).contacts;
-  var user = findContact(allcontacts, $scope.getQueryVar("uid"));
+  var data =  $lsGet("contactList") ? JSON.parse($lsGet("contactList")) : {
+    contacts : []
+  };
+  var user = findArrEl(data.contacts, $scope.getQueryVar("uid"));
   $scope.user = user;
-  // $scope.utrans = function () {
-  //   return tranList.transactions.filter((tran) => {
-  //     return tran.userid == user.id;
-  //   });
-  // }
+  var tranList =  $lsGet("tranList") ? JSON.parse($lsGet("tranList")) : {
+    transactions : []
+  };
+  function filterArr(arr, data){
+    var rdata = [];
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i].userid == data){
+        rdata.push(arr[i]);
+      }
+    }
+    return rdata;
+  }
+  var utransactions = filterArr(tranList.transactions, user.id);
+  console.log(utransactions);
+  $scope.utrans = utransactions;
   // $scope.debit = 0;
   // $scope.utrans().filter((tran) => {
   //   return tran.type == 1;
