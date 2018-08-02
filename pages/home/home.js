aee4db3da6c331d09c96f9da4812c23b0f472efc
rjabdurrahman <md.rahman0296@gmail.com>
2018-08-02 11:17:18 +0600

    Angular scoping and not supported problem solving started

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,47 +1,52 @@
-app.controller('HomeCntlr', function ($scope) {
+app.controller('HomeCntlr', function ($scope, $window) {
+  $scope.x = localStorage.getItem("name");
   $scope.message = 'Hello from HomeController';
-  $scope.htrans = tranList.transactions;
-  $scope.user = function (id) {
-    return data.contacts.find((user) => {
-      return user.id == id;
-    }).name;
-  }
-  $scope.trancol = function (type) {
-    if (type == -1)
-      return "red";
-  }
-  $scope.transym = function (type) {
-    if (type == -1)
-      return "–";
-  }
-  $scope.debit = 0;
-  $scope.htrans.filter((tran) => {
-    return tran.type == 1;
-  }).forEach((tran) => {
-    $scope.debit += tran.amount;
-  });
-  $scope.credit = 0;
-  $scope.htrans.filter((tran) => {
-    return tran.type == -1;
-  }).forEach((tran) => {
-    $scope.credit += tran.amount;
-  });
-  $scope.net = $scope.debit - $scope.credit;
-  if ($scope.debit > $scope.credit) {
-    $scope.debst = 100;
-    $scope.crest = Math.round(($scope.credit / $scope.debit) * 100);
-    $scope.netst = Math.round(($scope.net / $scope.debit) * 100);
-  }
-  else {
-    $scope.crest = 100;
-    $scope.debst = Math.round(($scope.debit / $scope.credit) * 100);
-    $scope.netst = Math.round(($scope.net / $scope.credit) * 100);
-  }
-  if ($scope.netst < 0) {
-    $scope.netcol = "red";
-    $scope.netst *= -1;
-  }
-  else {
-    $scope.netcol = "green";
-  }
+  localStorage.setItem("tranList", '{"transactions":[{"id":"100-0","userid":0,"type":1,"amount":210,"date":"","note":"efe","due":""}]}');
+  $scope.htrans = JSON.parse(localStorage.getItem("tranList")).transactions;
+  // $scope.user = function (id) {
+  //   return data.contacts.find((user) => {
+  //     return user.id == id;
+  //   }).name;
+  // }
+  // $scope.trancol = function (type) {
+  //   if (type == -1)
+  //     return "red";
+  // }
+  // $scope.transym = function (type) {
+  //   if (type == -1)
+  //     return "–";
+  // }
+  // $scope.debit = 0;
+  // $scope.htrans.filter((tran) => {
+  //   return tran.type == 1;
+  // }).forEach((tran) => {
+  //   $scope.debit += tran.amount;
+  // });
+  // $scope.credit = 0;
+  // $scope.htrans.filter((tran) => {
+  //   return tran.type == -1;
+  // }).forEach((tran) => {
+  //   $scope.credit += tran.amount;
+  // });
+  // $scope.net = $scope.debit - $scope.credit;
+  // if ($scope.debit > $scope.credit) {
+  //   $scope.debst = 100;
+  //   $scope.crest = Math.round(($scope.credit / $scope.debit) * 100);
+  //   $scope.netst = Math.round(($scope.net / $scope.debit) * 100);
+  // }
+  // else {
+  //   $scope.crest = 100;
+  //   $scope.debst = Math.round(($scope.debit / $scope.credit) * 100);
+  //   $scope.netst = Math.round(($scope.net / $scope.credit) * 100);
+  // }
+  // if ($scope.netst < 0) {
+  //   $scope.netcol = "red";
+  //   $scope.netst *= -1;
+  // }
+  // else {
+  //   $scope.netcol = "green";
+  // }
 });
+function myFun(){
+  return "Working";
+}
--- pages/userdetails/userdetails.js
+++ pages/userdetails/userdetails.js
@@ -1,6 +1,6 @@
-app.controller('UserDetailsCntlr', function ($scope) {
+app.controller('UserDetailsCntlr', function ($scope, $window) {
   $scope.getQueryVar = function (variable) {
-    var query = window.location.search.substring(1);
+    var query = $window.location.search.substring(1);
     var vars = query.split("&");
     for (var i = 0; i < vars.length; i++) {
       var pair = vars[i].split("=");
@@ -9,34 +9,41 @@ app.controller('UserDetailsCntlr', function ($scope) {
     return (false);
   };
   console.log($scope.getQueryVar("uid"));
-  var user = data.contacts.find((user) => {
-    return user.id == $scope.getQueryVar("uid");
-  });
+  function findContact(arr, data) {
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i].id == data){
+        return arr[i];
+        break;
+      }
+    }
+  }
+  var allcontacts = JSON.parse(localStorage.getItem("contactList")).contacts;
+  var user = findContact(allcontacts, $scope.getQueryVar("uid"));
   $scope.user = user;
-  $scope.utrans = function () {
-    return tranList.transactions.filter((tran) => {
-      return tran.userid == user.id;
-    });
-  }
-  $scope.debit = 0;
-  $scope.utrans().filter((tran) => {
-    return tran.type == 1;
-  }).forEach(function(tran) {
-    $scope.debit += tran.amount;
-  });
-  $scope.credit = 0;
-  $scope.utrans().filter((tran) => {
-    return tran.type == -1;
-  }).forEach(function(tran) {
-    $scope.credit += tran.amount;
-  });
-  $scope.trancol = function (type) {
-    if (type == -1)
-      return "red";
-  }
-  $scope.transym = function (type) {
-    if (type == -1)
-      return "–";
-  }
+  // $scope.utrans = function () {
+  //   return tranList.transactions.filter((tran) => {
+  //     return tran.userid == user.id;
+  //   });
+  // }
+  // $scope.debit = 0;
+  // $scope.utrans().filter((tran) => {
+  //   return tran.type == 1;
+  // }).forEach(function(tran) {
+  //   $scope.debit += tran.amount;
+  // });
+  // $scope.credit = 0;
+  // $scope.utrans().filter((tran) => {
+  //   return tran.type == -1;
+  // }).forEach(function(tran) {
+  //   $scope.credit += tran.amount;
+  // });
+  // $scope.trancol = function (type) {
+  //   if (type == -1)
+  //     return "red";
+  // }
+  // $scope.transym = function (type) {
+  //   if (type == -1)
+  //     return "–";
+  // }
   //End
 });
